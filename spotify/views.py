bc7cf13d9820daf35953ee017e9895ea489e08cf
avinash-vk <avinash2000vk@gmail.com>
2020-04-24 21:06:22 +0530

    more and more bugs

--- spotify/views.py
+++ spotify/views.py
@@ -18,7 +18,8 @@ uri2 = 'http://127.0.0.1:8000/'
 scopes = 'playlist-modify-public user-read-private'
 api_url_base = 'https://api.spotify.com/v1/playlists/'
 def add_playlist(request):
-    
+    x = request.user.spotify_user.all()[0]
+    username = x.spotify_id
     api_token = util.prompt_for_user_token(username, scopes,client_id,client_secret,redirect_uri)
     new_playlist = None
     if request.method == 'POST':
@@ -40,8 +41,8 @@ def add_playlist(request):
         new_playlist.save()
     return redirect('dashboard')
 def save_library(request,playlist_id):
-   
-    username = "1qvw6ha3tuv6icx9k78c7g5d3"
+    x = request.user.spotify_user.all()[0]
+    username = x.spotify_id
     playlist=get_object_or_404(Playlist, playlist_id=playlist_id,author = request.user)
     token = util.prompt_for_user_token(username, scopes,client_id,client_secret,redirect_uri)
     if token:
